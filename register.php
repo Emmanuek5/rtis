03a8662bfb87ce7a0a68bb422a475aa4132ec934
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2021-12-13 10:52:00 +0100

    Andrew's  8th Commit

--- register.php
+++ register.php
@@ -1,44 +1,35 @@
-<?php  
+<?php
 session_start();
 include 'config.php';
 
 if (isset($_POST['session'])) {
-	
-  $name = $_POST['name'];
-  $password1 = $_POST['password'];
-  $password = password_hash($password1,PASSWORD_DEFAULT);
-  $email = $_POST['email'];
-  $filename = $_FILES['file']['name'];
-  $file_tmp = $_FILES['file']['tmp_name'];
-  $file_new_name = time().date('dmy').$_FILES['file']['name'];
-  $location = './uploads/'.$file_new_name;
 
-  $quer = "SELECT `name` FROM `users` WHERE `name` = '$name'";
-  $result = mysqli_query($con,$quer);
+	$name = $_POST['name'];
+	$password1 = $_POST['password'];
+	$password = password_hash($password1, PASSWORD_DEFAULT);
+	$email = $_POST['email'];
+	$filename = $_FILES['file']['name'];
+	$file_tmp = $_FILES['file']['tmp_name'];
+	$file_new_name = time() . date('dmy') . $_FILES['file']['name'];
+	$location = './uploads/' . $file_new_name;
 
-
-
-if (mysqli_num_rows($result) > 0) {
-	echo('User Already Exists');
-}else{
- $user_id = rand();
-
- $sql = "INSERT INTO `users`( `user_id`, `name`,`password`,`email`,`image`) VALUES ('$user_id','$name','$password','$email','$file_new_name')";
- move_uploaded_file($file_tmp,$location);
-
- mysqli_query($con,$sql);
-
- header('location: login.php');
-
-}
-
- 
+	$quer = "SELECT `name` FROM `users` WHERE `name` = '$name'";
+	$result = mysqli_query($con, $quer);
 
 
 
- 
+	if (mysqli_num_rows($result) > 0) {
+		echo ('User Already Exists');
+	} else {
+		$user_id = rand();
 
- 
+		$sql = "INSERT INTO `users`( `user_id`, `name`,`password`,`email`,`image`) VALUES ('$user_id','$name','$password','$email','$file_new_name')";
+		move_uploaded_file($file_tmp, $location);
+
+		mysqli_query($con, $sql);
+
+		header('location: login.php');
+	}
 }
 
 
@@ -53,28 +44,28 @@ if (mysqli_num_rows($result) > 0) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./css/register.css">
+
 	<title></title>
 </head>
+
 <body>
 	<form method="POST" enctype="multipart/form-data">
-		<input type="text" name="name">
-		<input type="password" name="password">
-		<input type="email" name="email">
-		<input type="file" name="file" accept=".jpg,.png,.jpeg,.gif">
-		<input type="submit" name="session">
-
+		<label>Name</label>
+		<br><input type="text" name="name">
+		<br><label > Password</label>
+		<br><input type="password" name="password">
+		<br><label> Email</label>
+		<br><input type="email" name="email"><br>
+		<input type="file" name="file" accept=".jpg,.png,.jpeg,.gif"><br>
+		<input type="submit" name="session"><br>
+		<a href="./login.php">Login</a>
 	</form>
-<a href="./login.php">Login</a>
+
 </body>
+
 </html>
-.http-header{
-text-decoration: none;
-text-transform: capitalize;
-}
-.http-header:visited{
-	color: royalblue;
-	
-}
